1fea699746e59c0ece3734c3ac5618e85f9e1c87
Kevin Firko <kfirko@bitcurve.com>
2021-11-30 22:33:29 -0500

    add ormconfig in api src/ + configure DatabaseModule to pull fro it

--- apps/api/src/modules/database/database.module.ts
+++ apps/api/src/modules/database/database.module.ts
@@ -5,14 +5,10 @@ import ormconfig from '../../ormconfig'
 import { QueryUtilsService } from './query-utils.service'
 
 /**
- * Load ormconfig and specify additional options that are specific to nestjs+typeorm.
- *
- * @see ormconfig.ts for doc comment re webpack - setting `autoLoadEntities` to `true` may be required for projects in a monorepo,
+ * Load typeorm-compatible ormconfig and specify additional options specific to @nestjs/typeorm.
  */
 const typeOrmModuleOptions: TypeOrmModuleOptions = {
   ...ormconfig,
-
-  // @starter - automatically load any entities that are injected to modules via `TypeOrmModule.forFeature()`
   autoLoadEntities: true,
 }
 
