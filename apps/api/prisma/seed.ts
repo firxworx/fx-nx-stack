ab1ec7c65a858caa39b39d729c4402765dd681e5
Kevin Firko <kfirko@bitcurve.com>
2022-04-07 14:20:26 -0400

    initial working/example prisma schema + seed boilerplate in api app

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,28 @@
+import { Prisma, PrismaClient } from '../src/generated/prisma-client' // default configuration: '@prisma/client'
+//  import type { User } from '../src/generated/prisma-client'
+// import Chance from 'chance'
+
+const client = new PrismaClient()
+// var chance = new Chance()
+
+async function seed() {
+  const user: Prisma.UserCreateInput = {
+    name: 'Admin User',
+    email: 'admin@example.com',
+  }
+
+  await client.user.create({ data: user })
+
+  // for (let i = 0; i < 1000; i++) {
+  //   await client.example.create({
+  //     data: {
+  //       title: chance.word({ length: 10 }),
+  //       description: chance.sentence(),
+  //       url: chance.url(),
+  //       ownerId: user.id,
+  //     },
+  //   })
+  // }
+}
+
+seed()
